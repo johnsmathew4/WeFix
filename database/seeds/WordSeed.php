e0b57d9528f2e8755f97a28e6b697e03a4319e25
JOHNS MATHEW <JOHNS MATHEW>
2017-04-02 23:54:36 +0530

    completedata

--- database/seeds/WordSeed.php
+++ database/seeds/WordSeed.php
@@ -13,13 +13,142 @@ class WordSeed extends Seeder
     public function run()
     {
         $w=new Word;
-        $w->word='good';
-        $w->weight=1;
+        $w->word='great';
+        $w->weight=2;
         $w->save();
 
         $w=new Word;
-        $w->word='bad';
-        $w->weight=-1;
+        $w->word='like';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='genious';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='mind blowing';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='wow';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='success';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='super';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='interest';
+        $w->weight=1;
+        $w->save();
+        
+        
+        
+        $w=new Word;
+        $w->word='outstanding';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='value for money';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='stunning';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='terrific';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='smart';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='trustful';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='perfect';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='phenomenal';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='wonderful';
+        $w->weight=2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='fantastic';
+        $w->weight=2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='fabulous';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='quick';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='best';
+        $w->weight=2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='great';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='great';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='powerful';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='pretty';
+        $w->weight=2;
         $w->save();
 
         $w=new Word;
@@ -28,13 +157,377 @@ class WordSeed extends Seeder
         $w->save();
 
         $w=new Word;
-        $w->word='terrible';
+        $w->word='impressive';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='innovative';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='legendary';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='master';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='chilling';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='amazing';
+        $w->weight=2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='exciting';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='marvelous';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='marvelous';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='happy';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='worth';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='better';
+        $w->weight=2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='good';
+        $w->weight=2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='love';
+        $w->weight=2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='nice';
+        $w->weight=2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='ok';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='creative';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='pleasant';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='remarkable';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='satisf';
+        $w->weight=1;
+        $w->save();
+        
+        
+        $w=new Word;
+        $w->word='fine';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='brilliant';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='friendly';
+        $w->weight=1;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='experience';
+        $w->weight=1;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='recommended';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='genuine';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='safe';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='secure';
+        $w->weight=1;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='clean';
+        $w->weight=1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='reliable';
+        $w->weight=1;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='average';
+        $w->weight=0;
+        $w->save();
+        
+        
+        
+        
+        
+        $w=new Word;
+        $w->word='confusing';
+        $w->weight=-1;
+        $w->save();
+
+
+
+
+
+        $w=new Word;
+        $w->word='bug';
+        $w->weight=-1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='nonsense';
+        $w->weight=-1;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='unsatisfact';
         $w->weight=-2;
         $w->save();
 
         $w=new Word;
-        $w->word='great';
-        $w->weight=2;
+        $w->word='imperfect';
+        $w->weight=1;
         $w->save();
+
+        $w=new Word;
+        $w->word='bad';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='worthless';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='useless';
+        $w->weight=-2;
+        $w->save();
+        
+        $w=new Word;
+        $w->word='stupid';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='fool';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='unhappy';
+        $w->weight=-2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='irritate';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='unrecommended';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='ugly';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='fail';
+        $w->weight=-2;
+        $w->save();
+
+
+
+        $w=new Word;
+        $w->word='mistake';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='terrible';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='refuse';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='issue';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='poor';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='awful';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='shit';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='hate';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='disappointed';
+        $w->weight=-2;
+        $w->save();
+        
+        $w=new Word;
+        $w->word='disaster';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='pathetic';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='unrealistic';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='refund';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='worst';
+        $w->weight=-2;
+        $w->save();
+
+        $w=new Word;
+        $w->word='angry';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='boring';
+        $w->weight=-1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='ridiculous';
+        $w->weight=-1;
+        $w->save();
+
+        $w=new Word;
+        $w->word='appalling';
+        $w->weight=-2;
+        $w->save();
+
+
+        $w=new Word;
+        $w->word='waste';
+        $w->weight=-2;
+        $w->save();
+
+
+
+
+
     }
 }
